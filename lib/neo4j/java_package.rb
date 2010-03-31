f06e2ee70f48877e7047968f0cfce470af0bed78
Andreas Ronge <andreas.ronge@gmail.com>
2010-03-31 22:50:33 +0200

    You can now load jar files by require them, just like JRuby

--- lib/neo4j/java_package.rb
+++ lib/neo4j/java_package.rb
@@ -1,12 +1,48 @@
-
 require 'rubygems'
 gem 'rjb'
 require 'rjb'
 
 # Loads the JVM with the given <tt>classpath</tt> and arguments to the jre.
 # All needed .jars should be included in <tt>classpath</tt>.
-def load_jvm(classpath, jargs)
-  Rjb::load(classpath, jargs)
+
+module Kernel
+  alias rjb_original_require require
+
+  def require(path)
+    rjb_original_require(path)    
+  rescue LoadError
+     # check that it's not a jar file
+    raise unless path =~ /\.jar/
+
+    # get the path from where it was required
+    local_path = File.expand_path(File.dirname(caller[0].sub(/:\d+:in.`.*?'/, '')))
+    load_paths = [local_path] + $LOAD_PATH[0...-1]
+    found_path = load_paths.find{|p| File.exist?(File.join(p,path))}
+    puts "FOUND PATH #{found_path}"
+    raise unless found_path
+    
+    abs_path = File.join(found_path, path)
+    # check that the file exists
+    raise unless  File.exist?(abs_path)
+
+    # try to load it using RJB
+    puts "ADD abs '#{abs_path}'"
+    @rjb_jars ||= []
+    @rjb_jars << abs_path unless @rjb_jars.include?(abs_path)
+    # TODO
+  end
+
+  def load_jvm(jargs)
+    classpath = ENV['CLASSPATH'] ||= ''
+    @rjb_jars.each do |jar|
+      classpath += File::PATH_SEPARATOR unless classpath.empty?
+      classpath += jar
+    end
+#    classpath += File::PATH_SEPARATOR + File.join(libpath, 'neo4j-kernel-1.0.jar')
+#    classpath += File::PATH_SEPARATOR + File.join(libpath, 'geronimo-jta_1.1_spec-1.1.1.jar')
+    puts "LOAD JVM WITH '#{classpath}'"
+    Rjb::load(classpath, jargs)
+  end
 end
 
 class JavaPackage
@@ -20,13 +56,15 @@ class JavaPackage
 
   def method_missing(m, *args)
     # return if possible old module/class
+    puts "m=#{m} #{@cache[m]}"
     @cache[m] ||= create_package_or_class(m)
   end
 
   def create_package_or_class(m)
     method = m.to_s
+    puts "create package #{m}"
     if upcase?(method)
-      puts "Import #{self}.#{method}"
+#      puts "Import #{self}.#{method}"
       Rjb::import("#{self}.#{method}")
     else
       JavaPackage.new(method, self)
@@ -48,9 +86,7 @@ class JavaPackage
 
   @@cache = {}
    def self.new(pack_name, parent_pack = nil)
-      return @@cache[pack_name] if !parent_pack && @@cache[pack_name]
-      puts "INIT #{pack_name}"
-      super
+      @@cache[pack_name] ||= super
   end
 end
 
--- lib/neo4j/rjb.rb
+++ lib/neo4j/rjb.rb
@@ -1,34 +1,27 @@
 require 'java_package'
 require 'fileutils' # just for cleaning test data
 
-libpath = File.expand_path(File.join(File.dirname(__FILE__), 'jars'))
-puts "LIBPATH #{libpath}"
+require 'jars/neo4j-kernel-1.0.jar'
+require 'jars/geronimo-jta_1.1_spec-1.1.1.jar'
 
-classpath = ENV['CLASSPATH'] ||= ''
-classpath += File::PATH_SEPARATOR + File.join(libpath, 'neo4j-kernel-1.0.jar')
-classpath += File::PATH_SEPARATOR + File.join(libpath, 'geronimo-jta_1.1_spec-1.1.1.jar')
-
-puts "LOAD JVM"
-load_jvm(classpath, ['-Xms128m', '-Xmx1024m'])
-
-puts "create graph db"
+load_jvm(['-Xms128m', '-Xmx1024m'])
 
 FileUtils.rm_rf('/tmp/foo')
-db = org.neo4j.kernel.EmbeddedGraphDatabase.new('/tmp/foo') 
+
+puts "\nCreate DB"
+db = org.neo4j.kernel.EmbeddedGraphDatabase.new('/tmp/foo')
 
 puts "Create db"
 tx = db.beginTx
 puts "created tx = #{tx}"
 
 node = db.createNode
-puts "Created node #{node}"
-puts "Node id #{node.getId} "
+puts "Created node #{node} with id #{node.getId}"
 
 node.setProperty("name", "andreas")
 node.setProperty("age", 40)
 
 name = node.getProperty("name")
-
 puts "Name #{name._classname} #{name.toString}"
 age = node.getProperty("age")
 puts "Age #{age._classname}, #{age.intValue}"
