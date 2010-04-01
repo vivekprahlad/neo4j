9f81b2c0aabf2122a00e50a4b3a5d748ddbb490c
Andreas Ronge <andreas.ronge@gmail.com>
2010-04-02 00:36:57 +0200

    Added test file which demonstrates what works from CRuby/RJB and what doesn't work yet.

---
+++ lib/test.rb
@@ -0,0 +1,30 @@
+require 'neo4j'
+Neo4j.start
+Neo4j::Transaction.new
+a = Neo4j::Node.new
+b = Neo4j::Node.new :name => 'b'
+a[:name] = 'a'
+
+
+a.rels.outgoing(:friends) << b
+a.rels.each {|x| puts "Rel #{x.getEndNode[:name]}"}
+
+class PersonX
+  include Neo4j::NodeMixin
+  has_n :friends
+  property :name
+  # lucene index does not yet work does not work because lucene does not work with RJB
+  # Need to access in RJB -  org.apache.lucene.document.Field::Store::YES : org.apache.lucene.document.Field::Store::NO 
+end
+
+p1 = PersonX.new :name => 'p1'
+p2 = PersonX.new :name => 'p2'
+
+puts p1.name
+puts p2.name
+
+# the following line does not work since RJB need to load an enum in an Inner class -  org.neo4j.graphdb.Traverser::Order::BREADTH_FIRST
+# Should be possible to solve, check RJB mailing list for Enum
+#p1.friends << p2
+
+Neo4j::Transaction.finish
